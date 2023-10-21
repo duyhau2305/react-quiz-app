114f3f725f676b964b07e6901b0fb778d5afb833
unknown <duy2305@gmail.com>
2023-10-21 09:53:47 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import DashBoard from './assets/pages/DashBoard'
+
+function App() {
+  
+
+  return (
+    <div>
+     <DashBoard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/pages/DashBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DashBoard() {
+  return (
+    <div>
+        QUIZ APP
+    </div>
+
+  )
+}
+
+export default DashBoard
