7fabb282776eaebff903c803fcd04901f82ad286
Tony Nguyen <tony.nguyen@usonialabs.com>
2023-11-18 09:16:00 +0700

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { Suspense, lazy } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 
-import './App.css'
-import DashBoard from './assets/pages/DashBoard'
+// import DashBoard from './pages/DashBoard'
+// import Question from './pages/Question'
+// import Score from './pages/Score'
+
+const DashBoard = lazy(() => import('./pages/DashBoard'));
+const Question = lazy(() => import('./pages/Question'));
+const Score = lazy(() => import('./pages/Score'));
 
 function App() {
-  
-
   return (
     <div>
-     <DashBoard />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<DashBoard />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/score" element={<Score />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
