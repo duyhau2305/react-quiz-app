e179a2d8bde783d8ce7d383e9635713e761cdadd
Hau <duyhau2305@gmail.com>
2023-11-19 00:39:46 +0700

    Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { Suspense, lazy } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-// import DashBoard from './pages/DashBoard'
-// import Question from './pages/Question'
-// import Score from './pages/Score'
 
 const DashBoard = lazy(() => import('./pages/DashBoard'));
 const Question = lazy(() => import('./pages/Question'));
@@ -12,15 +9,16 @@ const Score = lazy(() => import('./pages/Score'));
 function App() {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<DashBoard />} />
+      <Suspense fallback={<div>Loading...</div>}> 
+        <Routes> 
+          <Route path="/" element={<DashBoard />}/>
           <Route path="/question" element={<Question />} />
           <Route path="/score" element={<Score />} />
+          
         </Routes>
       </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,12 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, Typography, Button, Select, MenuItem, TextField, InputLabel, FormControl } from '@mui/material';
 
 function DashBoard() {
-  return (
-    <div>
-        QUIZ APP
-    </div>
+  const navigate = useNavigate(); 
+    const [category, setCategory] = useState('');
+  const [difficulty, setDifficulty] = useState('');
+  const [type, setType] = useState('');
+  const [questionAmount, setQuestionAmount] = useState('');
 
-  )
+  
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleDifficultyChange = (event) => {
+    setDifficulty(event.target.value);
+  };
+
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+  };
+
+  const handleQuestionAmountChange = (event) => {
+    setQuestionAmount(event.target.value);
+  };
+
+  
+  const handleSubmit = () => {
+    
+    console.log(category, difficulty, type, questionAmount);    
+    navigate('/question');
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h2" component="h1" gutterBottom>
+        Quiz App
+      </Typography>
+
+      <FormControl fullWidth margin="normal">
+        <InputLabel id="category-label">Category</InputLabel>
+        <Select
+          labelId="category-label"
+          value={category}
+          label="Category"
+          onChange={handleCategoryChange}
+        >
+          <MenuItem value="science">Science</MenuItem>
+          <MenuItem value="math">Math</MenuItem>
+          {/* ... thêm các options khác */}
+        </Select>
+      </FormControl>
+
+      <FormControl fullWidth margin="normal">
+        <InputLabel id="difficulty-label">Difficulty</InputLabel>
+        <Select
+          labelId="difficulty-label"
+          value={difficulty}
+          label="Difficulty"
+          onChange={handleDifficultyChange}
+        >
+          <MenuItem value="easy">Easy</MenuItem>
+          <MenuItem value="medium">Medium</MenuItem>
+          <MenuItem value="hard">Hard</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl fullWidth margin="normal">
+        <InputLabel id="type-label">Type</InputLabel>
+        <Select
+          labelId="type-label"
+          value={type}
+          label="Type"
+          onChange={handleTypeChange}
+        >
+          <MenuItem value="multiple-choice">Multiple Choice</MenuItem>
+          <MenuItem value="true-false">True / False</MenuItem>
+        </Select>
+      </FormControl>
+
+      <TextField
+        fullWidth
+        margin="normal"
+        label="Amount of Questions"
+        value={questionAmount}
+        onChange={handleQuestionAmountChange}
+      />
+
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={handleSubmit} 
+        fullWidth
+        sx={{ marginTop: 2 }}
+      >
+        GET STARTED
+      </Button>
+    </Container>
+  );
 }
 
-export default DashBoard
+export default DashBoard;
